5af206d6a12a5e5002bfb35d752ba37eee27ebf5
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 11:29:58 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,44 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::process;
 
-fn not_found(command: &str) {
-    println!("{}: command not found", command);
-}
+fn handle_command(input: &str) -> bool {
 
-fn tokenize(input: &str) -> Vec<&str> {
-    input.split(' ').collect()
+    let parts: Vec<&str> = input.split_whitespace().collect();
+
+    match parts[0] {
+        "exit" => {
+            if parts.len() == 1 || (parts.len() > 1 && parts[1] == "0") {
+                return true;
+            } else {
+                println!("{}: command not found", input);
+            }
+        },
+        "echo" => println!("{}", &input[5..]),
+        "type" => {
+            if parts.len() > 1 && (parts[1] == "exit" || parts[1] == "echo" || parts[1] == "type") {
+                println!("{} is a shell builtin", parts[1]);
+            } else {
+                println!("{}: not found", parts[1]);
+            }
+        },
+        _ => println!("{}: command not found", input),
+    }
+
+    return false
 }
 
 fn main() {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+
     loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        if handle_command(input) {
+            break;
+        }
         print!("$ ");
         io::stdout().flush().unwrap();
-
-        let stdin = io::stdin();
-        let mut input = String::new();
-        stdin.read_line(&mut input).unwrap();
-        let command = input.trim();
-        let token = tokenize(command);
-        match token[..] {
-            ["exit", code] => process::exit(code.parse::<i32>().unwrap()),
-            ["echo", ..] => println!("{}", token[1..].join(" ")),
-            _ => not_found(command),
-        };
     }
 }

4823aedd16d4f68e1351bebc47bbc43b1da5db84
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 10:46:33 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 use std::io::{self, Write};
 
 fn main() {
-    let stdin = io::stdin();
-    let mut input = String::new();
-    
-    print!("$ ");
-    io::stdout().flush().unwrap();
-    while stdin.read_line(&mut input).is_ok() {
-        println!("{}: command not found", input.strip_suffix("\n").unwrap());
-        input.clear();
+    loop {
+        let stdin = io::stdin();
+        let mut input = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
+        stdin.read_line(&mut input).unwrap();
+
+        match input.trim().to_lowercase().as_str() {
+            "exit" => break,
+            "exit 0" => break,
+            _ => println!("{}: command not found", input.trim()),
+        };
     }
 }

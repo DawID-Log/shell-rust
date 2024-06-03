2c8ed9fac25eb5451616c5c9fd28246af8e90e95
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 13:38:33 +0200

    pass stage

--- README.md
+++ README.md
@@ -1,27 +1,8 @@
-[![progress-banner](https://backend.codecrafters.io/progress/shell/9b477b07-fd24-4fea-9e11-cf717465d97c)](https://app.codecrafters.io/users/codecrafters-bot?r=2qF)
-
-This is a starting point for Rust solutions to the
-["Build Your Own Shell" Challenge](https://app.codecrafters.io/courses/shell/overview).
-
-_Add a description of your course here_
-
-**Note**: If you're viewing this repo on GitHub, head over to
-[codecrafters.io](https://codecrafters.io) to try the challenge.
-
 # Passing the first stage
 
-The entry point for your `shell` implementation is in `src/main.rs`. Study and
-uncomment the relevant code, and push your changes to pass the first stage:
+The entry point for your `shell` implementation is in `src/main.rs`.
 
-```sh
-git add .
-git commit -m "pass 1st stage" # any msg
-git push origin master
-```
-
-Time to move on to the next stage!
-
-# Stage 2 & beyond
+# Stage 2
 
 Note: This section is for stages 2 and beyond.
 
@@ -31,3 +12,22 @@ Note: This section is for stages 2 and beyond.
    the first time you run it. Subsequent runs will be fast.
 1. Commit your changes and run `git push origin master` to submit your solution
    to CodeCrafters. Test output will be streamed to your terminal.
+
+I used WSL and I chose to use “Ubuntu”:
+```sh
+bash
+wsl --install -d Ubuntu
+```
+To choose the distro:
+```sh
+wsl --list --online
+```
+
+# Stage 3
+To start the shell, run:
+```sh
+bash
+exec cargo run --quiet --release --target-dir=/tmp/codecrafters-shell-target
+```
+
+# What is possible to do
--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,13 @@ fn handle_command(input: &str) -> bool {
     }
 
     match cmd {
+        "help" => {
+            println!("exit [n]: The exit utility shall cause the shell to exit from its current execution environment with the exit status specified by the unsigned decimal integer n. If the current execution environment is a subshell environment, the shell shall exit from the subshell environment with the specified exit status and continue in the environment from which that subshell environment was invoked; otherwise, the shell utility shall terminate with the specified exit status. If n is specified, but its value is not between 0 and 255 inclusively, the exit status is undefined.");
+            println!("\necho [string...]: The echo utility writes its arguments to standard output, followed by a <newline>. If there are no arguments, only the <newline> is written.");
+            println!("\ntype <command names>: The type command is used to describe how its argument would be translated if used as commands. It is also used to find out whether it is built-in or external binary file ");
+            println!("\ncd <directory>: The type command is used to describe how its argument would be translated if used as commands. It is also used to find out whether it is built-in or external binary file");
+            println!("\npwd: The pwd Linux command prints the current working directory path, starting from the root (/). Use the pwd command to find your way in the Linux file system structure maze or to pass the working directory in a Bash script.");
+        },
         "exit" => {
             if cmds.len() == 1 || (cmds.len() > 1 && args[0] == "0") {
                 return true;

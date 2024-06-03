ead2dc2e7648e1b0bba73096d62cb1c538046efd
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 11:53:09 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,45 @@
 use std::io::{self, Write};
 
 fn handle_command(input: &str) -> bool {
-
+    let path_env = std::env::var("PATH").unwrap();
+    let builtins = ["exit", "echo", "type"];
+    let argv = input.split_whitespace().collect::<Vec<&str>>();
+    if argv.is_empty() {
+        return true;
+    }
     let parts: Vec<&str> = input.split_whitespace().collect();
 
     match parts[0] {
         "exit" => {
             if parts.len() == 1 || (parts.len() > 1 && parts[1] == "0") {
-                return true;
+                return false;
             } else {
                 println!("{}: command not found", input);
             }
         },
         "echo" => println!("{}", &input[5..]),
         "type" => {
-            if parts.len() > 1 && (parts[1] == "exit" || parts[1] == "echo" || parts[1] == "type") {
-                println!("{} is a shell builtin", parts[1]);
+            if argv.len() != 2 {
+                println!("type: expected 1 argument, got {}", argv.len() - 1);
+                return false;
+            }
+            let cmd = argv[1];
+            if builtins.contains(&cmd) {
+                println!("{} is a shell builtin", cmd);
             } else {
-                println!("{}: not found", parts[1]);
+                println!("{} not found", cmd);
+                let split = &mut path_env.split(':');
+                if let Some(path) =
+                    split.find(|path| std::fs::metadata(format!("{}/{}", path, cmd)).is_ok())
+                {
+                    println!("{cmd} is {path}/{cmd}");
+                } else {
+                    println!("{cmd} not found");
+                }
             }
         },
-        _ => println!("{}: command not found", input),
+        _ => println!("{}: command not found", input.trim()),
     }
-
     return false
 }
 

cfc6034dea4e149e6687071a458a285ccb475634
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 11:05:17 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::process;
+
+fn not_found(command: &str) {
+    println!("{}: command not found", command);
+}
+
+fn tokenize(input: &str) -> Vec<&str> {
+    input.split(' ').collect()
+}
 
 fn main() {
     loop {
-        let stdin = io::stdin();
-        let mut input = String::new();
         print!("$ ");
         io::stdout().flush().unwrap();
-        stdin.read_line(&mut input).unwrap();
 
-        match input.trim().to_lowercase().as_str() {
-            "exit" => break,
-            "exit 0" => break,
-            _ => println!("{}: command not found", input.trim()),
+        let stdin = io::stdin();
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let command = input.trim();
+        let token = tokenize(command);
+        match token[..] {
+            ["exit", code] => process::exit(code.parse::<i32>().unwrap()),
+            ["echo", ..] => println!("{}", token[1..].join(" ")),
+            _ => not_found(command),
         };
     }
 }

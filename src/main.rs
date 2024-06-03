af09758a6e663154d242c82e524d1be2d2d5d662
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 10:38:12 +0200

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let mut input = String::new();
     
     print!("$ ");
+    io::stdout().flush().unwrap();
     while stdin.read_line(&mut input).is_ok() {
         println!("{}: command not found", input.strip_suffix("\n").unwrap());
         input.clear();
